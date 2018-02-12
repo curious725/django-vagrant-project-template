91a29965ef7db2ad74005c1802c500e95e896a88
Yurii Halapup <curious725@gmail.com>
2018-02-12 16:11:05 -0500

    add alias for quick activating virtualenv

--- provision/dev_provision.sh
+++ provision/dev_provision.sh
@@ -3,6 +3,11 @@
 cd /vagrant/{{ project_name }}/provision
 . base_provision.sh
 
+# add alias for quick work with virtualenv only if it's not already in ~/.bashrc
+grep -q -F 'alias cool="source /vagrant/{{ project_name }}/venv/bin/activate;cd /vagrant/{{ project_name }}/src"' ~/.bashrc || \
+echo 'alias cool="source /vagrant/{{ project_name }}/venv/bin/activate;cd /vagrant/{{ project_name }}/src"' >> ~/.bashrc && \
+source ~/.bashrc
+
 echo "hello from dev env"
 pwd
 
